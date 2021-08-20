ee9db57cb2e3fac1d8bae26f474e744b384c089c
R4ym0ndq <raymondquitua@gmail.com>
2021-08-21 01:47:50 +0800

    add flex-grow

--- components/Header.js
+++ components/Header.js
@@ -99,7 +99,7 @@ function Header({ placeholder }) {
                 </div>
             
         {searchInput && (
-            <div className="flex flex-wrap flex-col col-span-3 justify-center">
+            <div className="flex-grow flex-col col-span-3 justify-center">
 
                     {/* // p-2 mt-2 bg-gray-500
                     // rounded-b-lg rounded-t-sm shadow-xl lg:w-auto " */}
